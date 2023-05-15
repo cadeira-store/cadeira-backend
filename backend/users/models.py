42db221f87ca57cf1a74b90031bee71b272ab068
Александра Кайдалова <alexandrakaydalova@yandex.ru>
2023-05-15 17:26:49 +0300

    user model v1

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,32 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+
+from validators import phone_number_regex, validate_real_name
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Вход почта и пароль
+    """
+    email = models.EmailField(
+        'Email', max_length=settings.EMAIL_MAX_LENGTH, unique=True)
+    first_name = models.CharField(
+        'Имя', max_length=settings.USER_MAX_LENGTH,
+        validators=[validate_real_name], blank=False)
+    phone_number = models.CharField(
+        validators=[phone_number_regex],
+        max_length=16,
+        unique=True)
+    password = models.CharField(
+        'Пароль', max_length=settings.USER_MAX_LENGTH)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name']
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.name
